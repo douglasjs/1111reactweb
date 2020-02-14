608708ef6a04a811a6719b370fb6fcdffad32ed4
Benedik <wbenedik@gmail.com>
2020-02-14 22:17:47 +0800

    與資料庫連動，Script的引入仍有問題

--- src/components/customerweb/customerweb.js
+++ src/components/customerweb/customerweb.js
@@ -3,54 +3,90 @@ import TitleSet from '../sharecomponents/titleset';
 import CustomerHeader from './customerheader';
 import CustomerMain from './customermain';
 import CustomerFoot from './customerfoot';
-import useScript from '../sharecomponents/usescript';
+//import useScript from '../sharecomponents/usescript';
 
 
-function CustomerWeb(props) {
-    const cid = props.match ? props.match.params.cid : props.cid ? props.cid : '' ;
-    const companyName = '1111人力銀行-' + cid;
-    const companyMeta = {
-        keywords: '1111,永豐餘生技,人力,徵才',
-        description: '永豐餘生技(股)公司是永豐餘投資控股股份有限公司旗下關係企業，創立於民國 89 年，主要從事食品生技業、有機農業、製造業、餐飲業等。&nbsp;1998年永豐餘生物科技在臺灣宜蘭成立了南澳豐園農場，投身有機的事業，致力於栽培各種有機的食材，積極推廣尊重自然的有機生活哲學，試圖扮演有機生活管家的角色。'
+class CustomerWeb extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={};
     }
-    const styleSheet = [
-        '/assets/vendors/liquid-icon/liquid-icon.min.css',
-        '/assets/vendors/font-awesome/css/font-awesome.min.css',
-        '/assets/css/theme-vendors.min.css',
-        '/assets/css/theme.min.css',
-        '/assets/css/style_blue.css'
-    ]
-    const script = ['/assets/vendors/modernizr.min.js','/assets/vendors/jquery.min.js'];
+    
+    componentDidMount(){
+        const cid = this.props.match.params.cid;
+        this.props.getDataList(cid);
+    }
 
-    const URL = [
-        '/assets/vendors/jquery.min.js',
-        '/assets/js/theme-vendors.js',
-        '/assets/js/theme.min.js',
-        '/assets/js/liquidAjaxContactForm.min.js',
-        '/assets/js/liquidAjaxMailchimp.min.js',
-        '/assets/js/goTopButton.js'
-       ]
+    render(){
+        //const cid = this.props.match.params.cid;
+        const { data } = this.props.datatableReducer;
+     
+        let description = this.state.description;
+        let keyword = this.state.keyword ;
+        let title = this.state.title;
+        let styleType = this.state.styleType;    
+        
+        if(data && data.length > 0){
+            data.forEach(element => {
+                description =  element.description;
+                keyword = element.keyword;
+                title = element.title;
+                styleType = element.styleType;
+            })
+        }
 
-    useScript(URL);
+        //const cid = props.match ? props.match.params.cid : props.cid ? props.cid : '' ;
+        const webTitle = '1111人力銀行-' + title;
+        const companyMeta = {
+            keywordsMeta: keyword,
+            descriptionMeta: description
+        }
+        const styleSheet = [
+            '/assets/vendors/liquid-icon/liquid-icon.min.css',
+            '/assets/vendors/font-awesome/css/font-awesome.min.css',
+            '/assets/css/theme-vendors.min.css',
+            '/assets/css/theme.min.css',
+            '/assets/css/' + styleType
+        ]
+        const script = ['/assets/vendors/modernizr.min.js','/assets/vendors/jquery.min.js'];
+
+        /*const URL = [
+            '/assets/vendors/jquery.min.js',
+            '/assets/js/theme-vendors.js',
+            '/assets/js/theme.min.js',
+            '/assets/js/liquidAjaxContactForm.min.js',
+            '/assets/js/liquidAjaxMailchimp.min.js',
+            '/assets/js/goTopButton.js'
+        ]
+
+        useScript(URL);*/
 
 
-    return (
-         
-        <div>
-            <TitleSet title={companyName} meta={companyMeta} style={styleSheet} script={script} />
-            <div id="wrap">
-                <CustomerHeader {...props}/>
-                <CustomerMain {...props}/>
-                <CustomerFoot {...props}/>
-            </div>
-            <a className="top-link-icon hide" href="/#" id="js-top-icon"><i className="fa fa-angle-up"></i></a>
-            <a className="top-link hide" href="/#" id="js-top">
-                <img src="assets/img/join_icon_blue.png" alt="立即應徵" />
-            </a>
-        </div>    
-      
-    )
+        return (
+            
+            <div>
+                <TitleSet title={webTitle} meta={companyMeta} style={styleSheet} script={script} />
+                <div id="wrap">
+                    <CustomerHeader {...this.props}/>
+                    <CustomerMain {...this.props}/>
+                    <CustomerFoot {...this.props}/>
+                </div>
+                <a className="top-link-icon hide" href="/#" id="js-top-icon"><i className="fa fa-angle-up"></i></a>
+                <a className="top-link hide" href="/#" id="js-top">
+                    <img src="assets/img/join_icon_blue.png" alt="立即應徵" />
+                </a>
 
+                <script src="assets/vendors/jquery.min.js"></script>
+                <script src="assets/js/theme-vendors.js"></script>
+                <script src="assets/js/theme.min.js"></script>
+                <script src="assets/js/liquidAjaxContactForm.min.js"></script>
+                <script src="assets/js/liquidAjaxMailchimp.min.js"></script>
+                <script src="assets/js/goTopButton.js"></script>
+            </div>    
+        
+        )
+    }
 }
 
 export default CustomerWeb;
