9cf34fb430a8959a10d868e6f3913f01000ac576
TsaiBenedik <wbenedik@gmail.com>
2020-02-14 20:44:01 +0800

    change footer description
    變更footer文字

--- src/components/customerweb/customerfoot.js
+++ src/components/customerweb/customerfoot.js
@@ -1,7 +1,6 @@
 ﻿import React from 'react';
 
 function CustomerFoot() {
- 
 
     return (
         <footer className="lqd-main-footer-bk bg-cape-cod">
@@ -9,7 +8,12 @@ function CustomerFoot() {
                 <div className="container">
                     <div className="row">
                         <div className="lqd-column col-md-12 text-md-right text-white mt-20">
-                            <p className="my-0"><span>© 2019 1111人力銀行.</span></p>
+                            <p className="my-0"><span>
+                                上述內容由永豐餘生技提供並授權1111使用，內容含各公司與產品均分屬各相關公司與個別擁有者之商標。
+                                本網站內容享有著作權，禁止侵害，違者必究。
+                                <br />
+                                © 2019 1111人力銀行.
+                            </span></p>
                         </div>
 					</div>
 				</div>
