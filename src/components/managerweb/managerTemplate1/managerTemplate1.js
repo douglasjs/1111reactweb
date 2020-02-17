d8ca7978e7f0fbbccd987c943685f6fc2b489e6d
Benedik <wbenedik@gmail.com>
2020-02-17 16:06:13 +0800

    預覽畫面button調整為確認發佈

--- src/components/managerweb/managerTemplate1/managerTemplate1.js
+++ src/components/managerweb/managerTemplate1/managerTemplate1.js
@@ -64,7 +64,7 @@ class ManagerTemplate1 extends React.Component{
                 <ManagerTemplate1Session6 {...this.props}/>
                 <ManagerTemplate1Session7 {...this.props}/>
                 <ManagerTemplate1Session8 {...this.props}/>
-                <a href={`/${cid}`} target='_blank'rel="noopener noreferrer"><button className='btn btn-primary'>預覽畫面</button></a>
+                <div className="mangerweb-pb-1"><a href={`/${cid}`} target='_blank'rel="noopener noreferrer"><button className='btn btn-primary'>確認發佈</button></a></div>
             </div>
           
         )
