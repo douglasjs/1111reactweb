ee2001a938fa105e1fd3ff49f5d2c8d2a94bb6ad
TsaiBenedik <wbenedik@gmail.com>
2020-04-06 14:39:29 +0800

    edit config
    edit config

--- src/config/env.js
+++ src/config/env.js
@@ -1,24 +1,39 @@
-/*const envConfig = {
+/* 1111 */
+/*
+const envConfig = {
     kind01API : 'https://www.1111.com.tw/webService/NET40/CompanyPage/API.ashx',
     kind00API : 'https://www.1111.com.tw/webService/NET40/CompanyPage/API.ashx',
-    WebAPI: 'http://benedik.ddns.net:8080/api'
-}*/
-
-/*
-const benedikEnv = {
-    kind01API : 'http://localhost:3000/api/1111_API_kind_1.json',
-    kind00API : 'http://localhost:3000/api/1111_API_kind_0.json',
     WebAPI: 'http://benedik.ddns.net:8080/api'
 }
 */
 
-const douglasEnv = {
+/* OLD Benedik */
+/* 
+const envConfig = {
+    kind01API : 'http://localhost:3000/api/1111_API_kind_1.json',
+    kind00API : 'http://localhost:3000/api/1111_API_kind_0.json',
+    WebAPI: 'http://benedik.ddns.net:8080/api'
+} 
+*/
+
+/* Benedik */
+const envConfig = {
+    kind01API : 'http://benedik.ddns.net:8080/api/GetApi',
+    kind00API : 'http://benedik.ddns.net:8080/api/GetApi',
+    WebAPI: 'http://benedik.ddns.net:8080/api'
+}
+
+
+/* Douglas */
+/*
+const envConfig = {
     kind01API : 'http://benedik.ddns.net:8080/api/GetApi',
     kind00API : 'http://benedik.ddns.net:8080/api/GetApi',
     WebAPI: 'https://localhost:44312/api'
 
 }
+*/
 
-const envConfig = douglasEnv;
+//const envConfig = douglasEnv;
 
 export default envConfig;
