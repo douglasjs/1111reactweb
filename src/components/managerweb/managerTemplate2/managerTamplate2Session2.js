f0df28c14758ad1c236bd81b627377843649b143
Benedik <wbenedik@gmail.com>
2020-04-20 23:18:12 +0800

    調整後台版型畫面

--- src/components/managerweb/managerTemplate2/managerTamplate2Session2.js
+++ src/components/managerweb/managerTemplate2/managerTamplate2Session2.js
@@ -202,8 +202,8 @@ class Session2 extends React.Component{
 
         return(
             <div className="card shadow mb-4">
-                <a href="#collapseCard2" className="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseCard2">
-                    <h6 className="m-0 font-weight-bold text-primary">設定區段-公司介紹</h6>
+                <a href="#collapseCard2" className="d-block card-header py-3 text-white bg-primary" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseCard2">
+                    <h6 className="m-0 font-weight-bold">設定區段-公司介紹</h6>
                 </a>
                 <div className="collapse collapsed" id="collapseCard2">
                     <div className="card-body">
