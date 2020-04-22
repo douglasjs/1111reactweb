50518f034e102f762bad285405713d8e779036ea
TsaiBenedik <wbenedik@gmail.com>
2020-04-22 10:43:15 +0800

    調整Session 4排版

--- src/components/managerweb/managerTemplate2/managerTamplate2Session4.js
+++ src/components/managerweb/managerTemplate2/managerTamplate2Session4.js
@@ -434,7 +434,7 @@ class Session4 extends React.Component{
                                                     <th>標籤</th>
                                                     <th>職稱</th>
                                                     <th>薪資  </th>
-                                                    <th>工作內容 </th>
+                                                    <th width="30%">工作內容 </th>
                                                     <th>地點</th>
         
                                                 </tr>
@@ -449,7 +449,7 @@ class Session4 extends React.Component{
                                                             <td>{this.positionGroupMapping(element.position_group)} </td>
                                                             <td className='text-left'>{element.position_name} </td>
                                                             <td className='text-left'>{element.position_salary} </td>
-                                                            <td className='text-left' width="30%">{element.position_matter.substring(0, 100)} </td>
+                                                            <td className='text-left line-4 width-100'>{element.position_matter.substring(0, 100)} </td>
                                                             <td>{element.position_workCity} </td>
                                                          
                                                         </tr>
