3760120e8da29e579af63aa1f58d5a414a6d171e
TsaiBenedik <wbenedik@gmail.com>
2020-05-02 11:04:38 +0800

    footer排版調整

--- src/components/customerweb/tp01/customerfoot.js
+++ src/components/customerweb/tp01/customerfoot.js
@@ -12,10 +12,9 @@ function CustomerFoot(props) {
                 <div className="container">
                     <div className="row">
                         <div className="lqd-column col-md-12 text-md-right text-white mt-20">
-                            <p className="my-0"><span>
-                                上述內容由{companyName}提供並授權1111使用，內容含各公司與產品均分屬各相關公司與個別擁有者之商標。
-                                本網站內容享有著作權，禁止侵害，違者必究。
-                                <br />
+                            <p className="my-0 text-center"><span>
+                                上述內容由{companyName}提供並授權1111使用，內容含各公司與產品均分屬各相關公司與個別擁有者之商標。<br />
+                                本網站內容享有著作權，禁止侵害，違者必究。<br />
                                 © 2019 1111人力銀行.
                             </span></p>
                         </div>
