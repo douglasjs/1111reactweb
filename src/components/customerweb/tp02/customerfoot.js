fa62c4504619058b57a1cf1b2c2dd651eab330c6
TsaiBenedik <wbenedik@gmail.com>
2020-05-23 23:51:41 +0800

    版型四(原版型五)前台串接 footer

--- src/components/customerweb/tp02/customerfoot.js
+++ src/components/customerweb/tp02/customerfoot.js
@@ -1,6 +1,11 @@
 ﻿import React from 'react';
 
-function CustomerFoot() {
+function CustomerFoot(props) {
+    const { data } = props.datatableReducer;
+    let companyName = '1111人力銀行' ;
+    if(data && data.length > 0){
+        companyName = data[0].companyName;
+    }
 
     return (
         <footer className="main-footer" style={{backgroundColor: "#333"}}>
@@ -8,7 +13,11 @@ function CustomerFoot() {
 
                 <div className="footer-bottom clearfix">
                     <div className="left-content pull-left">
-                        <div className="copyright" style={{color: "#fff"}}>上述內容由媽咪樂居家服務集團提供並授權1111使用，內容含各公司與產品均分屬各相關公司與個別擁有者之商標。 本網站內容享有著作權，禁止侵害，違者必究。c 2019 1111人力銀行.</div>
+                        <div className="copyright" style={{color: "#fff"}}>
+                            上述內容由{companyName}提供並授權1111使用，內容含各公司與產品均分屬各相關公司與個別擁有者之商標。
+                            本網站內容享有著作權，禁止侵害，違者必究。<br />
+                            1111人力銀行 All Rights Reserved
+                        </div>
                     </div>
 
                 </div>
