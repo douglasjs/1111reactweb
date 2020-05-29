3a236731ca4279212eea74a3df78120dfb941b27
TsaiBenedik <wbenedik@gmail.com>
2020-05-29 22:29:14 +0800

    調整我要應徵按鈕連結

--- src/components/customerweb/tp06/customermain.js
+++ src/components/customerweb/tp06/customermain.js
@@ -34,6 +34,7 @@ function CustomerMain(props){
             let logURL = "/image/logo-1111.png";
             let webURL = "https://1111.com.tw";
             let styleType ="blue.css";
+            let goButtonURL = "https://1111.com.tw";
 
             if(data && data.length > 0){
                 webTitle = data[0].title;
@@ -47,6 +48,7 @@ function CustomerMain(props){
                 logURL  =  `${envConfig.WebAPI}/image/${cid}?fileName=${data[0].logoImg}`;
                 webURL = `https://1111.com.tw/brand/${cid}/${data[0].companyEName}`;
                 styleType = data[0].styleType;
+                goButtonURL = `https://www.1111.com.tw/corp/${cid}/`
             }
 
             return (
@@ -63,7 +65,7 @@ function CustomerMain(props){
                     <CompanyContact themeNum={themeNum} email={email} {...props} />
 
                     <a className="top-link-icon show" id="js-top-icon" href="/#"><i className="fa fa-angle-up"></i></a>
-                    <a className="top-link show" href="https://www.1111.com.tw/corp/50750609/#c4" id="js-top" target='_blank' rel='noopener noreferrer'>
+                    <a className="top-link show" href={goButtonURL} id="js-top" target='_blank' rel='noopener noreferrer'>
                         <img src={`/assets/tp06/img/join_icon_${styleType}.png`} alt="立即應徵" />
                     </a>
 
