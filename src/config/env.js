d73d76eb61cdb6d6c377eae12d2032ac773613ef
TsaiBenedik <wbenedik@gmail.com>
2020-08-02 00:23:09 +0800

    設定檔修改

--- src/config/env.js
+++ src/config/env.js
@@ -3,16 +3,16 @@
 const envConfig = {
     kind01API : 'http://192.168.1.195:8080/api/GetApi',
     kind00API : 'http://192.168.1.195:8080/api/GetApi',
-    email: 'http://192.168.1.195:8080/WebAPI/api/email',
+    email: 'http://192.168.1.195:8080/api/email',
     WebAPI: ' http://192.168.1.195:8080/api'
 }*/
 
 /* 1111 Public 
 const envConfig = {
-    kind01API : 'https://brand.1111.com.tw/WebAPI/api/GetApi',
-    kind00API : 'https://brand.1111.com.tw/WebAPI/api/GetApi',
-    email: 'https://brand.1111.com.tw/WebAPI/api/email',
-    WebAPI: 'https://brand.1111.com.tw/WebAPI/api'
+    kind01API : 'https://brand.1111.com.tw/api/GetApi',
+    kind00API : 'https://brand.1111.com.tw/api/GetApi',
+    email: 'https://brand.1111.com.tw/api/email',
+    WebAPI: 'https://brand.1111.com.tw/api'
 
 }*/
 
