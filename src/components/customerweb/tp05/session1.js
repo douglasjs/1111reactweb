ca002d01d5f807103e757ba56b47e9dc6e150325
TsaiBenedik <wbenedik@gmail.com>
2020-08-14 14:04:58 +0800

    版型五主視覺標題限制一排

--- src/components/customerweb/tp05/session1.js
+++ src/components/customerweb/tp05/session1.js
@@ -66,8 +66,8 @@ class CompanyTitle extends React.Component{
                                 <div className="d-table-cell text-center">
                                     <div className="container">
                                         <div className="banner-content s1_banner-content">
-                                            <h4 className="text-white tp05_shadow">{companySubTitle2}</h4>
-                                            <h1 className="tp05_shadow">{title2}</h1>
+                                            <h4 className="text-white tp05_shadow tp05_line-1">{companySubTitle2}</h4>
+                                            <h1 className="tp05_shadow tp05_line-1">{title2}</h1>
                                             <a href={companyurl} className="default-btn pt-10 tp05_hover-btn" target="_blank" rel="noopener noreferrer">我要應徵</a>
                                         </div>
                                     </div>
